5d5b5d885daf1ee5ff6a70d42699c5157bda06f2
Joshua Thomas K <109951862+URK21CS3005@users.noreply.github.com>
2022-07-31 21:27:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,17 +5,18 @@ class Evaluate:
       size_of_stack: An integer which represents the size of stack.
       stack: A List which acts as a Stack.
   """
-    # Write your code here
 
-
-  def _init_(self, size):
+  def __init__(self, size):
+    
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
-      size_of_stack: An integer to set the size of stack.
+      top:An integer which points to the top most element in the stack.
+      size_of_stack: An integer which represents size of stack.
+      stack: A list which maintians the elements of stack.
     """
     self.top = -1
-    self.size = size
-    self.lst = [None]*size
+    self.size_of_stack = size
+    self.stack = []
 
 
   def isEmpty(self):
@@ -24,42 +25,30 @@ class Evaluate:
     Returns:
       True if it is empty, else returns False.
     """
-    # Write your code here
     if self.top == -1:
-       return 1
-    else :
-       return 0
-    
-  def is_full(self):
-    # Write code here
-    if self.top == (self.size - 1):
-      return 1
-    else :
-      return 0
-    
+      return True
+    else:
+      return False
+
+
   def pop(self):
     """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
-    # Write your code here
     if not self.isEmpty():
-      z=self.lst[self.top]
-      del self.lst[self.top]
-      self.top-=1
-      return z
+      self.stack.pop()
 
-  def push(self, operand): 
+
+  def push(self, operand):
     """
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
     """
-    # Write your code here
-    if not self.is_full():
-      self.top+=1
-      self.lst[self.top]=operand
+    if self.top != self.size_of_stack - 1:
+      self.stack.append(operand)
 
 
   def validate_postfix_expression(self, expression):
@@ -70,18 +59,17 @@ class Evaluate:
     Returns:
       True if the expression is valid, else returns False.
     """
-    # Write your code here
-    count_1=0
-    count_2=0
-    for i in expression:
-      if i.isdigit():
-        count_1+=1
+    n = 0
+    o = 0
+    for element in expression:
+      if element.isnumeric():
+        n = n + 1
       else:
-        count_2+=1
-    if count_1>count_2 and expression[0].isdigit() and expression[1].isdigit():
-      return 1
+        o = o + 1
+    if o == n - 1:
+      return True
     else:
-      return 1
+      return False
 
 
   def evaluate_postfix_expression(self, expression):
@@ -92,21 +80,28 @@ class Evaluate:
     Returns:
       The result of evaluated postfix expression.
     """
-    # Write your code here
+    stack = []
     for i in expression:
-      if i.isdigit():
-        self.push(i)
-      else:
-        var_1 = self.pop()
-        var_2 = self.pop()
-        if i=='/':
-          self.push(str(eval(var_2 + i*2 + var_1)))
-        else:
-          self.push(str(eval(var_2 + i + var_1)))
-    return self.pop()
+      if i.isnumeric():
+        stack.append(int(i))
+      if len(stack) >= 2:
+        if i == '+':
+          stack[-2] = stack[-2] + stack[-1]
+          stack.pop()
+        elif i == '-':
+          stack[-2] = stack[-2] - stack[-1]
+          stack.pop()
+        elif i == '*':
+          stack[-2] = stack[-2] * stack[-1]
+          stack.pop()
+        elif i == '/':
+          stack[-2] = stack[-2] / stack[-1]
+          stack.pop()
+        elif i == '^':
+          stack[-2] = stack[-2] ^ stack[-1]
+          stack.pop()
+    return int(stack[-1])
 
-
-# Do not change the following code
 postfix_expression = input()  # Read postfix expression
 tokens = postfix_expression.split()
 evaluate = Evaluate(len(tokens))
